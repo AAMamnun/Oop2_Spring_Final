983d36ac96b005987e81324d5b90022cd9f11458
AAMamnun <aamamnun1998@gmail.com>
2021-03-28 23:07:24 +0600

    Test Interface 2

---
+++ Test_Interface_2/Test_Interface_2/BasicBankInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Interface_2
+{
+    interface BasicBankInterface
+    {
+        void Deposit(int amount);
+        void Withdraw(int amount);
+    }
+}
---
+++ Test_Interface_2/Test_Interface_2/Current_account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Interface_2
+{
+    class Current_account : Account,BasicBankInterface
+    {
+        public int amount { get; set; }
+        public Current_account(string AccName, string AccNo, int balance) : base(AccName, AccNo, balance)
+        {
+        }
+
+        public override void Deposit(int amount)
+        {
+            balance += amount;
+        }
+
+        public override void Withdraw(int amount)
+        {
+            if (amount < balance)
+            {
+                balance -= amount;
+            }
+        }
+    }
+}
